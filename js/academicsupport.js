c902752eea62b32b9ee6f8b6c4a64b18035d3cab
MattEMoran <mamo2629@colorado.edu>
2021-10-28 23:48:56 -0600

    Added js to check for empty answers, started code for dynamic form creation

--- js/academicsupport.js
+++ js/academicsupport.js
@@ -1,3 +1,16 @@
 function onClick() {
     alert("Test1234.")
 }
+
+window.onload = function enableButton() {  
+    var button = document.getElementById('submit_button');
+    var form = document.getElementById("academicsupportfundform");
+    var checkboxes1 = form.elements["role"];
+    if(checkboxes1 != ("Other" || "Educator" || "Assistant Principal" || "Principal's Assistant or Main Office Mgr" || "Principal")) 
+    {       
+        button.disabled = true;
+    } else {
+    	button.disabled = false;
+    	enableButton();
+    }        
+}    
