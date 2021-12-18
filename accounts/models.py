0ae642aeb9fb43901e61b2d3c3dd9fac862fd3f4
jacobef <jacobef2@gmail.com>
2021-12-18 02:59:50 -0800

    fix CustomUser.get_user_type() return type typehint

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # This is the class that will represent each user on the website, instead of Django's default User model
 class CustomUser(AbstractUser):
-    def get_user_type(self) -> str:
+    def get_user_type(self) -> str | None:
         possible_user_types = ["applicant", "volunteer", "administrator", "site-admin"]
         for possible_user_type in possible_user_types:
             if self.groups.filter(name=possible_user_type).exists():
