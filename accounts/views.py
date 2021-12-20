48052aaff24ab883dee5bc00e78bb1be1b72e760
jacobef <jacobef2@gmail.com>
2021-12-19 21:57:13 -0800

    clarify difference between register and save_user functions

--- accounts/views.py
+++ accounts/views.py
@@ -5,11 +5,13 @@ from accounts.forms import CustomUserCreationForm
 from accounts.models import CustomUser
 
 
+# This is the "create account" page that the user sees
 def register(request):
     form: CustomUserCreationForm = CustomUserCreationForm()
     return render(request, "registration/register.html", {'form': form})
 
 
+# Saves the user into the database; called after the user submits their account info in register.html
 def save_user(request):
     form: CustomUserCreationForm = CustomUserCreationForm(request.POST)
     form.save()
