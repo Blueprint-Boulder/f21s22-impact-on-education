7054d14252c1bbcef7d2ef5f5bdc8796542d33ea
jacobef <jacobef2@gmail.com>
2022-01-24 00:39:44 -0700

    make URLs and URL names separated by dashes instead of underscores

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,21 +14,17 @@ urlpatterns = [
     # After logging out, sends the user to the URL specified by LOGOUT_REDIRECT_URL in django_root/settings.py
     path('logout/', LogoutView.as_view(), name='logout'),
 
-    path('password_change/',
-         # The reason this view is different from the others: By default, PasswordChangeView's success_url is simply
-         #  reverse_lazy('password_change_done'). But in our case, password_change_done is namespaced under "accounts",
-         #  because it's in the "accounts" app, so we have to specify it as
-         #  reverse_lazy('accounts:password_change_done').
+    path('password-change/',
          PasswordChangeView.as_view(
              template_name="accounts/password_change_form.html",
-             success_url=reverse_lazy('accounts:password_change_done')
+             success_url=reverse_lazy('accounts:password-change-done')
          ),
-         name='password_change'),
-    path('password_change/done/',
+         name='password-change'),
+    path('password-change/done/',
          PasswordChangeDoneView.as_view(
              template_name="accounts/password_change_done.html"
          ),
-         name='password_change_done'),
+         name='password-change-done'),
 
     path('register/', views.register, name="register"),
     path('save-user/', views.save_user, name="save-user"),
