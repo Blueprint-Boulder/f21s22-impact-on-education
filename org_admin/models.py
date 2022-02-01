986fb19707c6d5f01f820247bd53127677de1b87
bryceschumacher <bryce.schumacher8@gmail.com>
2022-02-01 14:49:42 -0700

    add application equivalent for org admin

--- org_admin/models.py
+++ org_admin/models.py
@@ -1,4 +1,21 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
+from base_applicant.models import Application, School
 # Create your models here.
+
+
+
+class ScholarshipCreation(Application):
+    """Represents an org admin's creation of a scholarship."""
+    high_school = models.ForeignKey(School, on_delete=models.CASCADE)
+    statement = models.FileField()  # TODO: figure out what happens when upload_to=None (which is the default)
+    transcript = models.FileField()
+    recommendation_letter_1 = models.FileField()
+    recommendation_letter_2 = models.FileField()
+    acknowledged = models.BooleanField(default=False)
+
+
+    def get_absolute_url(self):
+        return reverse("student:orghome", kwargs={'pk': self.pk})
--- org_admin/views.py
+++ org_admin/views.py
@@ -1,18 +1,89 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.http import HttpResponse
 
 from base_applicant.models import Application
 from accounts.models import CustomUser
+
+from base_applicant.views import ApplicationCreateView, ApplicationUpdateView, ApplicationDeleteView, \
+    base_submit_application, base_confirm_submit_application, base_view_applications
+from org_admin.models import ScholarshipCreation
+
+
+
+
+# Below is all Creation of new scholarship application
+
+class ScholarshipApplicationCreateViewAdmin(ApplicationCreateView):
+    model = ScholarshipCreation
+    fields = ApplicationCreateView.fields + ['high_school',
+                                             'statement',
+                                             'transcript',
+                                             'recommendation_letter_1', 'recommendation_letter_2',
+                                             'acknowledged']
+    template_name = "org_admin/application_form.html"
+
+
+class ScholarshipApplicationUpdateViewAdmin(ApplicationUpdateView):
+    model = ScholarshipCreation
+    fields = ApplicationUpdateView.fields + ['high_school',
+                                             'statement',
+                                             'transcript',
+                                             'recommendation_letter_1', 'recommendation_letter_2',
+                                             'acknowledged']
+    template_name = "org_admin/application_form.html"
+
+
+class ScholarshipApplicationDeleteViewAdmin(ApplicationDeleteView):
+    model = ScholarshipCreation
+    success_url = reverse_lazy('student:view-apps')  # goes here after confirmation
+    template_name = "org_admin/application_confirm_delete.html"  # confirmation page
+
 
 
 def is_admin(user: CustomUser):
     """Checks whether a user is an org admin"""
     return user.account_type == CustomUser.AccountTypes.ORG_ADMIN
 
-
 # Users table
 @user_passes_test(is_admin)
 def users(request):
     """A view that is a table of every user on the website, and relevant info about them."""
     return render(request, "org_admin/users.html", {'users': CustomUser.objects.all()})
+
+def add_application(request):
+    return render(request, "org_admin/applicationadd.html")
+
+
+def home(request):
+    """View for the student homepage."""
+    return render(request, "org_admin/orgadmin_home.html", {'user': request.user})
+
+
+def view_application(request, pk: int):
+    """View for students to see one of their applications, in a read-only format."""
+    application: ScholarshipCreation = ScholarshipCreation.objects.get(pk=pk)
+    # TODO (high priority): Make into actual page
+    return HttpResponse(f"SCHOLARSHIP APPLICATION<br>ID {application.pk}")
+
+
+def view_applications(request):
+    """View for students to see all of their applications, in a read-only format."""
+    return base_view_applications(request,
+                                  application_class=ScholarshipCreation,
+                                  template_name="org_admin/applications_list.html")
+
+
+def confirm_submit_application(request, pk: int):
+    """Confirmation page before submitting an application."""
+    return base_confirm_submit_application(request, pk=pk,
+                                           application_class=ScholarshipCreation,
+                                           template_name="org_admin/application_confirm_submit.html")
+
+
+def submit_application(request, pk: int):
+    return base_submit_application(request, pk=pk,
+                                   application_class=ScholarshipCreation,
+                                   success_url=reverse("org_admin:view-apps"))
