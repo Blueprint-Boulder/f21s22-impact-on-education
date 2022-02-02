57ffc8ce1aad6cc07994a10fc3e3cfba15990613
digh9649 <digh9649@colorado.edu>
2022-02-02 10:53:28 -0700

    base of form is finished, need to add CSS

--- student/views.py
+++ student/views.py
@@ -64,3 +64,7 @@ def submit_application(request, pk: int):
     return base_submit_application(request, pk=pk,
                                    application_class=ScholarshipApplication,
                                    success_url=reverse("student:view-apps"))
+
+
+def academic_funding_form(request):
+    return render(request, "student/academic_funding_form.html")
