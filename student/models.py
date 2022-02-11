49afe2017c9cf3d4209797558da158110859f20d
jacobef <jacobef2@gmail.com>
2022-02-11 03:30:25 -0700

    small cleanup

--- student/models.py
+++ student/models.py
@@ -1,22 +1,15 @@
-import os
-
-from django.core.files import File
-from django.core.files.storage import FileSystemStorage
 from django.db import models
-from django.db.models import FileField, QuerySet
-from django.db.models.fields.files import FieldFile
+from django.db.models import FileField
 from django.urls import reverse
-from django.utils.functional import cached_property
 
 from accounts.models import CustomUser
 from base_applicant.models import Application, School
-from django_root import settings
 
 
 class ScholarshipApplication(Application):
     """Represents a student's application for a scholarship."""
     high_school = models.ForeignKey(School, on_delete=models.CASCADE)
-    statement = FileField()  # TODO: figure out what happens when upload_to=None (which is the default)
+    statement = FileField()
     transcript = FileField()
     recommendation_letter_1 = FileField()
     recommendation_letter_2 = FileField()
